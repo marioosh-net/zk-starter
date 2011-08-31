319ff31ac513d1033bef7857fe69e5a286d91c14
marioosh <marioosh.net@gmail.com>
2011-08-31 12:25:06 +0200

    akcja na buttonie

--- src/main/java/net/marioosh/zkstarter/DemoWindowComposer.java
+++ src/main/java/net/marioosh/zkstarter/DemoWindowComposer.java
@@ -3,10 +3,15 @@ package net.marioosh.zkstarter;
 import org.apache.log4j.Logger;
 import org.zkoss.zk.ui.Component;
 import org.zkoss.zk.ui.util.GenericForwardComposer;
+import org.zkoss.zul.api.Textbox;
 
 public class DemoWindowComposer extends GenericForwardComposer {
 	
 	private Logger log = Logger.getLogger(getClass());
+	
+	private Textbox textbox;
+	
+	private int times;
 	
 	public void doAfterCompose(Component comp) throws Exception {
 		super.doAfterCompose(comp);
@@ -14,5 +19,7 @@ public class DemoWindowComposer extends GenericForwardComposer {
 	
 	public void onClick$okButton() {
 		log.debug("click");
+		times++;
+		textbox.setText("Clicked " + times + " times.");
 	}
 }
