d047a1e9e26b6db35aab803f5070b8935a3edd36
marioosh <marioosh.net@gmail.com>
2011-08-31 15:33:44 +0200

    integracja ze spring mvc

---
+++ src/main/java/net/marioosh/zkstarter/SpringService.java
@@ -0,0 +1,12 @@
+package net.marioosh.zkstarter;
+
+import java.util.Date;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class SpringService {
+	public Date getDate() {
+		return new Date();
+	}
+}
