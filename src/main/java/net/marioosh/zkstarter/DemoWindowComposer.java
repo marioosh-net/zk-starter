9df56630b6d8a86821c15ab94ec4c2eee20eebc8
marioosh <marioosh.net@gmail.com>
2011-08-31 12:18:30 +0200

    ZK Licence Version: ZK 5 CE

--- src/main/java/net/marioosh/zkstarter/DemoWindowComposer.java
+++ src/main/java/net/marioosh/zkstarter/DemoWindowComposer.java
@@ -1,12 +1,18 @@
 package net.marioosh.zkstarter;
 
+import org.apache.log4j.Logger;
 import org.zkoss.zk.ui.Component;
 import org.zkoss.zk.ui.util.GenericForwardComposer;
 
 public class DemoWindowComposer extends GenericForwardComposer {
 	
+	private Logger log = Logger.getLogger(getClass());
+	
 	public void doAfterCompose(Component comp) throws Exception {
 		super.doAfterCompose(comp);
-		
+	}
+	
+	public void onClick$okButton() {
+		log.debug("click");
 	}
 }
