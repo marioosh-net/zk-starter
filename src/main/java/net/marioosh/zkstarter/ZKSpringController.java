ede52f9e01f731694827d7b9f483c0ea8dfe94b8
marioosh <marioosh.net@gmail.com>
2011-09-01 10:01:36 +0200

    autowired works good

--- src/main/java/net/marioosh/zkstarter/ZKSpringController.java
+++ src/main/java/net/marioosh/zkstarter/ZKSpringController.java
@@ -1,5 +1,10 @@
 package net.marioosh.zkstarter;
 
+import java.util.List;
+
+import net.marioosh.zkstarter.model.dao.UserDAO;
+import net.marioosh.zkstarter.model.entities.User;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.zkoss.spring.context.annotation.EventHandler;
@@ -12,15 +17,20 @@ import org.zkoss.zul.Button;
 
 @Component("zkSpringController")
 public class ZKSpringController extends GenericSpringComposer {
+
+	@Autowired
+	private Button button;
+
+	@Autowired
+	private Textbox textbox;
 	
 	@Autowired
-    private Button button;
-	
-	 @Autowired
-	 private Textbox textbox;
-	
+	private SpringService springService;
+
 	@EventHandler("button.onClick")
-    public void showGreeting(Event evt) throws WrongValueException, InterruptedException {
-        Messagebox.show("Hello " + textbox.getValue() + "!");
-    }
+	public void showGreeting(Event evt) throws WrongValueException,
+			InterruptedException {
+		Messagebox.show("Hello " + textbox.getValue() + "!\nActual date: "+ springService.getDate());
+	}
+
 }
