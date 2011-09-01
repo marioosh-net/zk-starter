e476c66a21d1df977ef76f006abeed5ddfece78c
marioosh <marioosh.net@gmail.com>
2011-09-01 09:06:26 +0200

    przyklad wykorzystania SpringUtil

--- src/main/java/net/marioosh/zkstarter/DemoWindowComposer.java
+++ src/main/java/net/marioosh/zkstarter/DemoWindowComposer.java
@@ -3,6 +3,7 @@ package net.marioosh.zkstarter;
 import org.apache.log4j.Logger;
 import org.zkoss.zk.ui.Component;
 import org.zkoss.zk.ui.util.GenericForwardComposer;
+import org.zkoss.zkplus.spring.SpringUtil;
 import org.zkoss.zul.api.Textbox;
 
 public class DemoWindowComposer extends GenericForwardComposer {
@@ -13,13 +14,17 @@ public class DemoWindowComposer extends GenericForwardComposer {
 	
 	private int times;
 	
+	/**
+	 * SpringUtil - jeden ze sposobow na dostanie sie do beanow Springa
+	 */
+	private final SpringService springService = (SpringService) SpringUtil.getBean("springService");	
+	
 	public void doAfterCompose(Component comp) throws Exception {
 		super.doAfterCompose(comp);
 	}
 	
 	public void onClick$okButton() {
 		log.debug("click");
-		times++;
-		textbox.setText("Clicked " + times + " times.");
+		textbox.setText("Clicked " + ++times + " times.");
 	}
 }
--- src/main/java/net/marioosh/zkstarter/SpringService.java
+++ src/main/java/net/marioosh/zkstarter/SpringService.java
@@ -6,16 +6,20 @@ import java.util.List;
 import net.marioosh.zkstarter.model.dao.UserDAO;
 import net.marioosh.zkstarter.model.entities.User;
 
+import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class SpringService {
 	
+	private Logger log = Logger.getLogger(getClass()); 
+	
 	@Autowired
 	private UserDAO userDAO;
 	
 	public Date getDate() {
+		log.debug("getDate()");
 		return new Date();
 	}
 	
